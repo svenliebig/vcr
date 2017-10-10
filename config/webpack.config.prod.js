d4217411f7fce154bcbc3b2c9ab33529489a9a1c
Sly321 <liebigsv@gmail.com>
2017-10-10 03:00:33 +0200

    update for build

--- config/webpack.config.prod.js
+++ config/webpack.config.prod.js
@@ -94,6 +94,9 @@ module.exports = {
       // Support React Native Web
       // https://www.smashingmagazine.com/2016/08/a-glimpse-into-the-future-with-react-native-for-web/
       'react-native': 'react-native-web',
+      '@service': paths.appSrc + '/service',
+      '@component': paths.appSrc + '/component',
+      '@model': paths.appSrc + '/model'
     },
     plugins: [
       // Prevents users from importing files from outside of src/ (or node_modules/).
