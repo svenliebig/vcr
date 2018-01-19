5395a2f0836ab4e8d6b4cbe74ccc0c462ec4cbbb
Sven Liebig <liebigsv@gmail.com>
2018-01-19 19:52:36 +0100

    update cd script

--- scripts/cd-build-prepare.js
+++ scripts/cd-build-prepare.js
@@ -1,15 +1,22 @@
 const fs = require("fs")
+const packagejson = __dirname + "/../package.json"
+const index = __dirname + "/../public/index.html"
 
-var pjson = require('../package.json');
+function replaceStringInFile(regex, replaceStr, file) {
+	fs.readFile(file, 'utf8', function (err,data) {
+		if (err) {
+			return console.log(err)
+		}
+		var result = data.replace(regex, replaceStr)
 
-const index = __dirname + "/../public/index.html";
-fs.readFile(index, 'utf8', function (err,data) {
-  if (err) {
-    return console.log(err);
-  }
-  var result = data.replace(/<title>VCR<\/title>/g, `<title>VCR - ${pjson.version}<\/title>`);
+		fs.writeFile(file, result, 'utf8', function (err) {
+			if (err) return console.log(err);
+		})
+	})
+}
 
-  fs.writeFile(index, result, 'utf8', function (err) {
-     if (err) return console.log(err);
-  });
-});
+var pjson = require('../package.json')
+
+
+replaceStringInFile(/<title>VCR<\/title>/g, `<title>VCR - ${pjson.version}<\/title>`, index)
+replaceStringInFile(/http:\/\/tv.slyox.de/g, `http://tv.beta.slyox.de`, packagejson)
