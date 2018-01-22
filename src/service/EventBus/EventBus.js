580848305f9f52f5f6078335ac515031cbd27818
Sven Liebig <liebigsv@gmail.com>
2018-01-23 00:40:00 +0100

    create an event bus

---
+++ src/service/EventBus/EventBus.js
@@ -0,0 +1,33 @@
+export default class EventBus {
+	constructor() {
+		this.events = {}
+	}
+
+	register(event, callback) {
+		if (Object.getOwnPropertyNames(this.events).indexOf(event) === -1) {
+			this.events[event] = callback
+		} else {
+			throw ("Allready registered emit event")
+		}
+	}
+
+	emit(event, ...args) {
+		if (Object.getOwnPropertyNames(this.events).indexOf(event) !== -1) {
+			this.events[event].apply(this, args)
+		}
+	}
+
+	/**
+	 * @returns {EventBus}
+	 */
+	static get instance() {
+		return this._instance
+	}
+
+	static set instance(instance) {
+		this._instance = instance
+	}
+
+}
+
+EventBus.instance = new EventBus()
--- src/service/index.js
+++ src/service/index.js
@@ -1,2 +1,3 @@
 export { default as Message } from "./Message"
 export { default as UserRepository } from "./user"
+export { default as EventBus } from "./EventBus"
