11a1ce1abe945d5e4d00542940e15b5afa0320d3
Sven Liebig <liebigsv@gmail.com>
2018-08-30 01:06:59 +0200

    rebase from master

--- config/aliases.js
+++ config/aliases.js
@@ -1,14 +1,15 @@
 const paths = require("./paths")
 
 const aliases = {
-    'react-native': 'react-native-web',
-    '@service': paths.appSrc + '/service',
-    '@components': paths.appSrc + '/components',
-    '@scenes': paths.appSrc + '/scenes',
-    '@environment': paths.appSrc + '/environment',
-    '@converter': paths.appSrc + '/converter',
-    '@model': paths.appSrc + '/model',
-    '@utils': paths.appSrc + '/utils'
+    "react-native": "react-native-web",
+    "@service": paths.appSrc + "/service",
+    "@components": paths.appSrc + "/components",
+    "@scenes": paths.appSrc + "/scenes",
+    "@environment": paths.appSrc + "/environment",
+    "@converter": paths.appSrc + "/converter",
+    "@model": paths.appSrc + "/model",
+    "@utils": paths.appSrc + "/utils",
+    "@details": paths.appSrc + "/details"
 }
 
 module.exports = aliases
---
+++ src/details/DetailsView.tsx
@@ -0,0 +1,23 @@
+import * as React from "react"
+
+export interface Props {
+    children?: React.ReactNode
+}
+
+export interface State {
+}
+
+export default class DetailsView extends React.Component<Props, State> {
+    constructor(props: Props) {
+        super(props)
+
+        this.state = {
+        }
+    }
+
+    render() {
+        return (
+            <div>{this.props.children}</div>
+        )
+    }
+}
